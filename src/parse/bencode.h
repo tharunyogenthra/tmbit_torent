045c7a9a5a6ef08e4d4e806c6f6564441c6f331e
Tharun Yogenthra <z5480907@ad.unsw.edu.au>
2024-09-28 12:06:09 +1000

    Added initial code that we needed to have anyway. Made the setup which includes importing catch2 and make a CMakeList.txt so we can build easier

---
+++ src/parse/bencode.h
@@ -0,0 +1,11 @@
+#ifndef tmbit_torrent_bencode_h
+#define tmbit_torrent_bencode_h
+
+class bencode_file {
+    // get fields methods
+    // to string
+    // decode
+    // read torrent file
+};
+
+#endif // tmbit_torrent_bencode_h
---
+++ src/parse/bencode.test.cpp
@@ -0,0 +1,5 @@
+#include <../lib/catch2/catch.hpp>
+
+TEST_CASE("Checking if tests work") {
+    CHECK(1 == 1);
+}
